ddd204ca89dd02b4234cbe6dc225957f7d709b02
stan-mqz <moscarstanley37@gmail.com>
2024-10-09 08:18:27 -0600

    Add HeaderProps type to resolve prop validation issue

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,18 @@
+import { CartItem, Guitar } from "../types/types"
+
+type HeaderProps = {
+  cart: CartItem[]
+  removeFromCart : (id: Guitar["id"]) => void
+  increaseQuantity : (id: Guitar["id"]) => void
+  decreaseQuantity : (id: Guitar["id"]) => void
+  clearCart :  () => void
+  isEmpty : boolean
+  cartTotal : number
 
 
-export default function Header({ cart, removeFromCart, increaseQuantity, decreaseQuantity, clearCart, isEmpty, cartTotal }) {
+}
+
+export default function Header({ cart, removeFromCart, increaseQuantity, decreaseQuantity, clearCart, isEmpty, cartTotal }: HeaderProps) {
 
   
   return (
--- src/types/types.ts
+++ src/types/types.ts
@@ -10,7 +10,7 @@ export type Guitar = {
 }
 
 
-export type CartItem = Pick <Guitar, 'id' | 'name' | 'price'> & {
+export type CartItem = Pick <Guitar, 'id'  | 'image' | 'name' | 'price'> & {
     quantity: number
 }
 
